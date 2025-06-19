cf62dd0edd0fb31c1646a055a4877d2f5184facb
Kvitdrake <efremmukonin3@gmail.com>
2025-06-19 21:57:57 +0300

    обновление запусковых файлов

--- webb_tst_site3/render-build.sh
+++ webb_tst_site3/render-build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-echo "----- Building .NET application -----"
-cd ./webb_tst_site3
+echo "===== Установка .NET 8 ====="
+# Принудительная установка .NET
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version 8.0.100
+export PATH="$HOME/.dotnet:$PATH"
+
+echo "===== Пути проекта ====="
+pwd
+ls -la
+
+echo "===== Сборка .NET приложения ====="
+cd webb_tst_site3 || exit
+dotnet restore
 dotnet publish -c Release -o ../publish
--- webb_tst_site3/render-start.sh
+++ webb_tst_site3/render-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo "----- Starting application -----"
-cd ./publish
-dotnet webb_tst_site3.dll
+export PATH="$HOME/.dotnet:$PATH"
+echo "===== Запуск приложения ====="
+cd publish
+dotnet webb_tst_site3.dll --urls "http://0.0.0.0:$PORT"
